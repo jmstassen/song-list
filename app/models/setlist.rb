77cf979207096f265081c831219fcce3366156ea
John M. Stassen <john.stassen@gmail.com>
2021-04-09 16:05:46 -0700

    complete setlist model and setlists migration

--- app/models/setlist.rb
+++ app/models/setlist.rb
@@ -1,2 +1,6 @@
 class Setlist < ApplicationRecord
+    has_many :song_selections
+    has_many :setlist_permissions
+    has_many :songs, through: :song_selections
+    has_many :users, through: :setlist_permissions
 end
