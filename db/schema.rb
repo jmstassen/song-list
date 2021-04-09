e139c700c102b5b89140e8e1c9ae055050c4cbe4
John M. Stassen <john.stassen@gmail.com>
2021-04-09 16:14:31 -0700

    run rake db:migrate

---
+++ db/schema.rb
@@ -0,0 +1,83 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_04_09_225322) do
+
+  create_table "lines", force: :cascade do |t|
+    t.text "lyrics"
+    t.string "chords"
+    t.integer "line_order"
+    t.integer "song_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["song_id"], name: "index_lines_on_song_id"
+  end
+
+  create_table "setlist_permissions", force: :cascade do |t|
+    t.integer "permission"
+    t.integer "user_id"
+    t.integer "setlist_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["setlist_id"], name: "index_setlist_permissions_on_setlist_id"
+    t.index ["user_id"], name: "index_setlist_permissions_on_user_id"
+  end
+
+  create_table "setlists", force: :cascade do |t|
+    t.string "title"
+    t.string "location"
+    t.date "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "song_selections", force: :cascade do |t|
+    t.integer "song_order"
+    t.integer "song_id"
+    t.integer "setlist_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["setlist_id"], name: "index_song_selections_on_setlist_id"
+    t.index ["song_id"], name: "index_song_selections_on_song_id"
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "title"
+    t.string "songwriter"
+    t.string "tempo"
+    t.string "key"
+    t.text "note"
+    t.string "link"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "user_notes", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.integer "song_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["song_id"], name: "index_user_notes_on_song_id"
+    t.index ["user_id"], name: "index_user_notes_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "display_name"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
