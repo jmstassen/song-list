0108a9ecce793299b5c9098d54821da63017aa2e
John M. Stassen <john.stassen@gmail.com>
2021-04-10 08:29:20 -0700

    define strong params for setlist and make a stub definition of create method in setlists controller

--- app/controllers/setlists_controller.rb
+++ app/controllers/setlists_controller.rb
@@ -4,4 +4,14 @@ class SetlistsController < ApplicationController
     @setlist = Setlist.new
   end
       
+  def create
+    binding.pry
+  end
+
+  private
+
+  def setlist_params
+    params.require(:setlist).permit(:title, :location, :date)
+  end
+
 end
