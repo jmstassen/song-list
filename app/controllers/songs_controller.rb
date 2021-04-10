4c4889c8b9758addc020c248497086d048e73201
John M. Stassen <john.stassen@gmail.com>
2021-04-10 10:52:23 -0700

    build new and create methods in SongsController and strong params

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,22 @@
 class SongsController < ApplicationController
+
+  def new
+    @song = Song.new
+  end
+          
+  def create
+    @song = current_user.songs.build(song_params)
+    if @song.save
+      redirect_to songs_path
+    else
+      render :new
+    end
+  end
+    
+  private
+    
+  def song_params
+    params.require(:song).permit(:title, :songwriter, :tempo, :key, :note, :link)
+  end
+
 end
