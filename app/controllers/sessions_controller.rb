4afd2bc4988de0c49401f988130dc8687a8c4dcf
John M. Stassen <john.stassen@gmail.com>
2021-04-09 18:23:50 -0700

    define create method in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,4 +7,16 @@ class SessionsController < ApplicationController
     session.clear
     redirect_to root_path
   end
+
+  def create
+    user = User.find_by(email: params[:user][:email])
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      flash[:message] = "Invalid combination of email and password"
+      redirect_to "/login"
+    end  
+  end
+
 end
