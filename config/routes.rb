aa544c4ae7ca998d2725fd0e741732c636505fc5
John M. Stassen <john.stassen@gmail.com>
2021-04-10 22:25:20 -0700

    draw google oauth callback route

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   post 'login' => 'sessions#create' 
 
   delete 'logout' => 'sessions#destroy'
+  
+  get "/auth/google_oauth2/callback" => 'sessions#google'
 
   resources :users do
     resources :setlists, only: [:new, :create, :index]
