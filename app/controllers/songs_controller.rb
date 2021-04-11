f73a7c962eef119ee7b879ba069f562f6ae20c81
John M. Stassen <john.stassen@gmail.com>
2021-04-10 19:53:03 -0700

    define show method, add lines attributes to strong params, create stub lines for new song

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,12 +2,23 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+    @song.lines.build(line_order: 1)
+    @song.lines.build(line_order: 2)
+    @song.lines.build(line_order: 3)
+    @song.lines.build(line_order: 4)
+    @song.lines.build(line_order: 5)
+    @song.lines.build(line_order: 6)
+    @song.lines.build(line_order: 7)
+    @song.lines.build(line_order: 8)
+    @song.lines.build(line_order: 9)
+    @song.lines.build(line_order: 10)
+    @song.lines.build(line_order: 11)
   end
           
   def create
     @song = Song.new(song_params)
     if @song.save
-      redirect_to songs_path
+      redirect_to song_path(@song)
     else
       render :new
     end
@@ -23,10 +34,27 @@ class SongsController < ApplicationController
     end
   end
 
+  def show
+    @song = Song.find_by_id(params[:id])
+    redirect_to '/' if !@song
+  end
+
   private
     
   def song_params
-    params.require(:song).permit(:title, :songwriter, :tempo, :key, :note, :link)
+    params.require(:song).permit(
+      :title,
+      :songwriter,
+      :tempo,
+      :key,
+      :note,
+      :link,
+      lines_attributes: [
+        :lyrics,
+        :chords,
+        :line_order
+      ]
+    )
   end
 
 end
