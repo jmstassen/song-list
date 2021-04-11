b4cbcdb17aa5c0d451c42e7018e1c9be7a0ed9d7
John M. Stassen <john.stassen@gmail.com>
2021-04-10 20:50:16 -0700

    remove ability for user to accept nested parameters for setlists

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,4 @@ class User < ApplicationRecord
   has_many :setlists, through: :setlist_permissions
   has_many :songs, through: :setlists
   has_secure_password
-  accepts_nested_attributes_for :setlists
 end
