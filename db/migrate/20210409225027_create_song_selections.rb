5c57bf9986a8cbf0b06b138154e225cac645ca29
John M. Stassen <john.stassen@gmail.com>
2021-04-15 20:25:10 -0700

    add foreign key: true for song and setlist in song_selections migration

--- db/migrate/20210409225027_create_song_selections.rb
+++ db/migrate/20210409225027_create_song_selections.rb
@@ -2,8 +2,8 @@ class CreateSongSelections < ActiveRecord::Migration[6.1]
   def change
     create_table :song_selections do |t|
       t.integer :song_order
-      t.references :song
-      t.references :setlist
+      t.references :song, foreign_key: true
+      t.references :setlist, foreign_key: true
 
       t.timestamps
     end
