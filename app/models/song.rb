200ef6bea01b337817959739c8a4dcef98946c91
John M. Stassen <john.stassen@gmail.com>
2021-04-15 20:23:32 -0700

    update song model: belongs_to user, no user notes

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,8 +1,8 @@
 class Song < ApplicationRecord
   has_many :lines
   has_many :song_selections
-  has_many :user_notes
   has_many :setlists, through: :song_selections
+  belongs_to :user
   accepts_nested_attributes_for :lines
 
   scope :alpha, -> { order(:title) }
