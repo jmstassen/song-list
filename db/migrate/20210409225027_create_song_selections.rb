fc991cbdb606ed495cb68aa56ca95d2a68a3f9be
John M. Stassen <john.stassen@gmail.com>
2021-04-15 20:27:11 -0700

    change column name from song_order to song_number

--- db/migrate/20210409225027_create_song_selections.rb
+++ db/migrate/20210409225027_create_song_selections.rb
@@ -1,7 +1,7 @@
 class CreateSongSelections < ActiveRecord::Migration[6.1]
   def change
     create_table :song_selections do |t|
-      t.integer :song_order
+      t.integer :song_number
       t.references :song, foreign_key: true
       t.references :setlist, foreign_key: true
 
