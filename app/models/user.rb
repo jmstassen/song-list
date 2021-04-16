c15f7ae923c2aac8f94f35f5fddbad9fe0a6ab1a
John M. Stassen <john.stassen@gmail.com>
2021-04-15 20:22:45 -0700

    update user model: no user notes, has many songs (not through: setlists)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
-  has_many :user_notes
+  has_many :songs
   has_many :setlist_permissions
   has_many :setlists, through: :setlist_permissions
-  has_many :songs, through: :setlists
   has_secure_password
 end
