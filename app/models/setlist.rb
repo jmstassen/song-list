caa0c0990471ef8534927cb51ae444958505a6a0
John M. Stassen <john.stassen@gmail.com>
2021-04-19 08:22:57 -0700

    remove setlist has_many :users, through :setlist_permissions

--- app/models/setlist.rb
+++ app/models/setlist.rb
@@ -2,7 +2,6 @@ class Setlist < ApplicationRecord
   has_many :song_selections
   has_many :setlist_permissions
   has_many :songs, through: :song_selections
-  has_many :users, through: :setlist_permissions
 
   scope :today_or_future, lambda { where("date >= ?", Date.today) }   
 
