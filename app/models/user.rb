ca42d975c07548df2d2891131cfb3ba7f95e6e87
John M. Stassen <john.stassen@gmail.com>
2021-05-30 20:01:25 -0700

    add active record validations for user: username, display_name, email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   has_many :songs, dependent: :destroy
   has_many :setlist_permissions
   has_many :setlists, through: :setlist_permissions
+  validates :username, uniqueness: true, format: { with: /\A[a-zA-Z0-9]+\Z/, message: "must contain only letters and/or numbers (and no spaces)" }
+  validates :display_name, presence: true
+  validates :email, presence: true, uniqueness: true
 
   has_secure_password
 end
