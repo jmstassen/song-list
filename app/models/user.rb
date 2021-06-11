9d9632711e39d5bbdf24bcb3202760ab5c094827
John M. Stassen <john.stassen@gmail.com>
2021-06-11 14:20:11 -0700

    remove validation on username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,8 @@ class User < ApplicationRecord
   has_many :songs, dependent: :destroy
   has_many :setlist_permissions
   has_many :setlists, through: :setlist_permissions
-  validates :username, uniqueness: true, format: { with: /\A[a-zA-Z0-9]+\Z/, message: "must contain only letters and/or numbers (and no spaces)" }
   validates :display_name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true
 
   has_secure_password
 end
