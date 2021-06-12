a534f8b49bf2b9f0720858eaef775d6e4ec0eb85
John M. Stassen <john.stassen@gmail.com>
2021-06-12 14:24:31 -0700

    validate setlist for title presence

--- app/models/setlist.rb
+++ app/models/setlist.rb
@@ -2,6 +2,7 @@ class Setlist < ApplicationRecord
   has_many :song_selections
   has_many :setlist_permissions
   has_many :songs, through: :song_selections
+  validates :title, presence: true
   
   scope :today_or_future, lambda { where("date >= ?", Date.today) }   
 
