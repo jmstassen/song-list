8779762c0f7d2f0812f5fec475acd51d6a8d2ef8
John M. Stassen <john.stassen@gmail.com>
2021-06-13 10:17:36 -0700

    add permit song_selections_attributes, create 10 blank song_selections  in new setlist

--- app/controllers/setlists_controller.rb
+++ app/controllers/setlists_controller.rb
@@ -2,9 +2,15 @@ class SetlistsController < ApplicationController
 
   def new
     @setlist = Setlist.new
+    n = 1
+    10.times do
+      @setlist.song_selections.build(:song_number => n)
+      n += 1
+    end
   end
       
   def create
+    binding.pry
     @setlist = Setlist.new(setlist_params)
     if @setlist.save
       SetlistPermission.create(:user_id => current_user.id, :setlist_id => @setlist.id, :permission => 0)
@@ -12,6 +18,10 @@ class SetlistsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit
+    @setlist = Setlist.find(params[:id])
   end
 
   def index
@@ -26,7 +36,16 @@ class SetlistsController < ApplicationController
   private
 
   def setlist_params
-    params.require(:setlist).permit(:title, :location, :date)
+    params.require(:setlist).permit(
+      :title, 
+      :location, 
+      :date, 
+      song_selections_attributes: [
+        :song_id, 
+        :setlist_id, 
+        :song_number
+      ]
+    )
   end
 
 end
