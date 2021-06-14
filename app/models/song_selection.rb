b2e551c321277c2db79676e04f38cda502753d22
John M. Stassen <john.stassen@gmail.com>
2021-06-14 11:40:37 -0700

    define song_title in song_selection model

--- app/models/song_selection.rb
+++ app/models/song_selection.rb
@@ -2,12 +2,8 @@ class SongSelection < ApplicationRecord
   belongs_to :song
   belongs_to :setlist
 
-  def song_title=(title)
-    self.song_title = Song.find_by(:id => self.song_id).title
-  end
-
   def song_title
-    self.song_title ? self.song.title : nil
+    self.song.title
   end
 
 end
