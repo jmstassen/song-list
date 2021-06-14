77504d01ea91455d2701221d176c01a68dc78c57
John M. Stassen <john.stassen@gmail.com>
2021-06-14 14:30:03 -0700

    use scope to alphabetize songs when passing to views

--- app/controllers/setlists_controller.rb
+++ app/controllers/setlists_controller.rb
@@ -1,7 +1,7 @@
 class SetlistsController < ApplicationController
 
   def new
-    @songs = current_user.songs.all
+    @songs = current_user.songs.all.alpha
     @setlist = Setlist.new
     n = 1
     10.times do
@@ -16,7 +16,7 @@ class SetlistsController < ApplicationController
       SetlistPermission.create(:user_id => current_user.id, :setlist_id => @setlist.id, :permission => 0)
       redirect_to setlist_path(@setlist)
     else
-      @songs = current_user.songs.all
+      @songs = current_user.songs.all.alpha
       render :new
     end
   end
