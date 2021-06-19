0de5117cb7ab8bc9b5ae202b247ff40897640896
John M. Stassen <john.stassen@gmail.com>
2021-06-18 21:35:00 -0700

    remove logic for setlist permission nested under user

--- app/controllers/setlist_permissions_controller.rb
+++ app/controllers/setlist_permissions_controller.rb
@@ -3,8 +3,6 @@ class SetlistPermissionsController < ApplicationController
   def index
     if params[:setlist_id] && @setlist = Setlist.find_by_id(params[:setlist_id])
       @setlist_permissions = @setlist.setlist_permissions
-    elsif params[:user_id] && @user = User.find_by_id(params[:user_id])
-      @setlist_permissions = @user.setlist_permissions
     else
       @setlist_permissions = SetlistPermissions.all
     end
