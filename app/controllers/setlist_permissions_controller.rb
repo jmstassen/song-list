b84519efb8b7b8976512deda3f3bbd393ed423b8
John M. Stassen <john.stassen@gmail.com>
2021-06-18 21:28:27 -0700

    define setlist_permissions#index

--- app/controllers/setlist_permissions_controller.rb
+++ app/controllers/setlist_permissions_controller.rb
@@ -1,7 +1,13 @@
 class SetlistPermissionsController < ApplicationController
 
   def index
-    @setlist_permissions = SetlistPermission.all
+    if params[:setlist_id] && @setlist = Setlist.find_by_id(params[:setlist_id])
+      @setlist_permissions = @setlist.setlist_permissions
+    elsif params[:user_id] && @user = User.find_by_id(params[:user_id])
+      @setlist_permissions = @user.setlist_permissions
+    else
+      @setlist_permissions = SetlistPermissions.all
+    end
   end
 
 end
