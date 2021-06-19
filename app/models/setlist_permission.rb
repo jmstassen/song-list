773214a044bff8e5c1374d6d689c979e7e46ac50
John M. Stassen <john.stassen@gmail.com>
2021-06-19 13:22:02 -0700

    define PERMISSIONS constant, write self.permissable(*args) method that defines methods for view, edit, and destroy permissions

--- app/models/setlist_permission.rb
+++ app/models/setlist_permission.rb
@@ -2,7 +2,24 @@ class SetlistPermission < ApplicationRecord
   belongs_to :user
   belongs_to :setlist
 
+  PERMISSIONS = {
+    :destroy => 0,
+    :edit => 10,
+    :view => 100
+  }
+  
+  def self.permissable(*args)
+    args.each do |action|
+      define_method "#{action}able?" do
+        self.permission <= PERMISSIONS[action]
+      end
+    end
+  end
+
+  permissable :view, :edit, :destroy
+  
   def user_email
     self.user.email
   end
+
 end
