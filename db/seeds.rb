c7d4c2c85e594bfa12aa7a52f7e4ee0633c00e07
John M. Stassen <john.stassen@gmail.com>
2021-06-25 16:25:06 -0700

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,12 @@
 
 users = User.create(
     [
-        { username: 'Andy', email: 'andy@andy.com', display_name: 'Display Andy', password: 'password'},
-        { username: 'Bette', email: 'bette@bette.com', display_name: 'Display Bette', password: 'password'},
-        { username: 'Charles', email: 'charles@charles.com', display_name: 'Display Charles', password: 'password'},
-        { username: 'Don', email: 'don@don.com', display_name: 'Display Don', password: 'password'},
-        { username: 'Edward', email: 'edward@edward.com', display_name: 'Display Edward', password: 'password'},
-        { username: 'Fred', email: 'fred@fred.com', display_name: 'Display Fred', password: 'password'}
+        { email: 'andy@andy.com', display_name: 'Display Andy', password: 'password'},
+        { email: 'bette@bette.com', display_name: 'Display Bette', password: 'password'},
+        { email: 'charles@charles.com', display_name: 'Display Charles', password: 'password'},
+        { email: 'don@don.com', display_name: 'Display Don', password: 'password'},
+        { email: 'edward@edward.com', display_name: 'Display Edward', password: 'password'},
+        { email: 'fred@fred.com', display_name: 'Display Fred', password: 'password'}
     ]
 )
 
