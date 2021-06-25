9c686e50a45a9ed352716d06367382bc86bea1d1
John M. Stassen <john.stassen@gmail.com>
2021-06-25 12:08:05 -0700

    add resources for setlist_permissions

--- config/routes.rb
+++ config/routes.rb
@@ -19,10 +19,12 @@ Rails.application.routes.draw do
   resources :setlists do
     resources :songs, only: [:new, :create, :index]
     resources :song_selections, only: [:new, :create, :index]
-    resources :setlist_permissions, only: [:index, :new, :create]
+    resources :setlist_permissions, only: [:index, :new, :create, :edit]
   end
 
   resources :songs
+
+  resources :setlist_permissions
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
