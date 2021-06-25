c3a6f75b44cd4c1229a0348894c382913a32ac34
John M. Stassen <john.stassen@gmail.com>
2021-06-25 12:06:10 -0700

    define edit, new, create methods and strong params

--- app/controllers/setlist_permissions_controller.rb
+++ app/controllers/setlist_permissions_controller.rb
@@ -1,11 +1,51 @@
 class SetlistPermissionsController < ApplicationController
 
+  PERMISSIONS = {
+    :destroy => 0,
+    :edit => 10,
+    :view => 100
+  }
+
   def index
     if params[:setlist_id] && @setlist = Setlist.find_by_id(params[:setlist_id])
       @setlist_permissions = @setlist.setlist_permissions
     else
       @setlist_permissions = SetlistPermissions.all
     end
+  end
+
+  def edit
+    @setlist_permission = SetlistPermission.find(params[:id])
+    @user = @setlist_permission.user_email
+    @permissions = PERMISSIONS
+  end
+
+  def new
+    @setlist_permission = SetlistPermission.new
+  end
+
+  def create 
+    @user = User.find_by(:email => params[:user_email])
+    if !@user
+      flash[:message] = "No such user exists."
+      redirect_to new_setlist_setlist_permission_path(params[:setlist_id])
+    else
+      @setlist_permission = SetlistPermission.create(
+        :user_id => @user.id, 
+        :permission => params[:permission], 
+        :setlist_id => params[:setlist_id]
+        )
+        redirect_to setlist_setlist_permissions_path(params[:setlist_id])
+    end
+  end
+
+  private
+  def setlist_permission_params
+    params.require(:setlist_permission).permit(
+      :user_email,
+      :permission,
+      :setlist_id
+    )
   end
 
 end
