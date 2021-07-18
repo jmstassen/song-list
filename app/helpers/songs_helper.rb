a063e547d71ff8ae8a7c23a61b5ddf344344df95
John M. Stassen <john.stassen@gmail.com>
2021-07-17 19:53:34 -0700

    create tempo_and_key helper to show based on what exists

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,2 +1,15 @@
 module SongsHelper
+
+  def tempo_and_key(song)
+    if song.tempo == "" && song.key == ""
+      return ""
+    elsif song.tempo == ""
+      return "Key: #{song.key}"
+    elsif song.key == ""
+      return "Tempo: #{song.tempo}"
+    else
+      return "Tempo: #{song.tempo} | Key: #{song.key}"
+    end
+  end
+
 end
