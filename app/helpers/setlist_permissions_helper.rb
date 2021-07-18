1d5f8cdb9bb8453119c0010248672c258cf7184e
John M. Stassen <john.stassen@gmail.com>
2021-07-17 21:36:30 -0700

    define show_hide_setlist_permission_edit

--- app/helpers/setlist_permissions_helper.rb
+++ app/helpers/setlist_permissions_helper.rb
@@ -1,2 +1,11 @@
 module SetlistPermissionsHelper
+
+  def show_hide_setlist_permission_edit(setlist_permission)
+    if setlist_permission.user_id == Setlist.find_by_id(setlist_permission.setlist_id).user_id
+      return "hide"
+    else
+      return ""
+    end
+  end
+
 end
