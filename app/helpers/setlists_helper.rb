fad00d7b1d7cac1fc09940a68bcf71239ea6ce04
John M. Stassen <john.stassen@gmail.com>
2021-07-17 21:37:02 -0700

    define methods to hide edit or delete links for setlists

--- app/helpers/setlists_helper.rb
+++ app/helpers/setlists_helper.rb
@@ -1,2 +1,19 @@
 module SetlistsHelper
+
+
+  def show_hide_edit(setlist)
+    if can_current_user?(:edit, setlist)
+      return ""
+    else
+      return "hide"
+    end
+  end
+
+  def show_hide_delete(setlist)
+    if can_current_user?(:destroy, setlist)
+      return ""
+    else
+      return "-hide"
+    end
+  end
 end
