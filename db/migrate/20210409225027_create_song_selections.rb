94933e3a4bee0665b2244aefbe0402637184891e
John M. Stassen <john.stassen@gmail.com>
2021-07-17 21:46:04 -0700

    add on_delete: :cascade for :setlist foreign key

--- db/migrate/20210409225027_create_song_selections.rb
+++ db/migrate/20210409225027_create_song_selections.rb
@@ -3,7 +3,7 @@ class CreateSongSelections < ActiveRecord::Migration[6.1]
     create_table :song_selections do |t|
       t.integer :song_number
       t.references :song, foreign_key: true
-      t.references :setlist, foreign_key: true
+      t.references :setlist, foreign_key: true, on_delete: :cascade
 
       t.timestamps
     end
--- db/migrate/20210409225239_create_setlist_permissions.rb
+++ db/migrate/20210409225239_create_setlist_permissions.rb
@@ -3,7 +3,7 @@ class CreateSetlistPermissions < ActiveRecord::Migration[6.1]
     create_table :setlist_permissions do |t|
       t.integer :permission
       t.references :user, foreign_key: true
-      t.references :setlist, foreign_key: true
+      t.references :setlist, foreign_key: true, on_delete: :cascade
 
       t.timestamps
     end
