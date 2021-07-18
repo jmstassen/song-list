9bcf236e09f66fef09e20c49508538044d3411d1
John M. Stassen <john.stassen@gmail.com>
2021-07-18 07:50:21 -0700

    remove commented out definition

--- app/controllers/song_selections_controller.rb
+++ app/controllers/song_selections_controller.rb
@@ -1,11 +1,2 @@
 class SongSelectionsController < ApplicationController
-
-#   def new
-#     if params[:setlist_id]
-#       @setlist = Setlist.find_by_id(params[:setlist_id])
-#     end
-#     @songs = current_user.songs.all
-#     @song_selection = SongSelection.new
-#   end
-
 end
